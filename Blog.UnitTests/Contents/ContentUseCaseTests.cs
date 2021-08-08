8bbdc43310ff24769a04dd45e362881e1fc9fd5c
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-08 14:09:20 -0300

    test(ContentUseCase): ensure it returns a list of content on success

--- Blog.UnitTests/Contents/ContentUseCaseTests.cs
+++ Blog.UnitTests/Contents/ContentUseCaseTests.cs
@@ -1,17 +1,33 @@
+using System.Threading.Tasks;
+
 using Xunit;
+using Moq;
+
+using Blog.Application.Contents.Protocols;
+using Blog.Application.Contents.UseCases;
+using System.Collections.Generic;
+using Blog.Domain.Contents.Entities;
 
 namespace Blog.UnitTests.Contents
 {
     public class ContentUseCaseTests
     {
         [Fact]
-        public void TestName()
+        public async Task ShouldReturnListOfContentsWhen()
         {
             //Given
-            
+            IEnumerable<Content> actual = new List<Content>();
+            var getContentListRepositoryMock = new Mock<IGetContentListRepository>(MockBehavior.Default);
+            var sut = new ContentUseCase(getContentListRepositoryMock.Object);
+
             //When
-            
+            //mock.Setup(foo => foo.DoSomethingAsync()).Returns(async () => 42);
+            getContentListRepositoryMock.Setup(method => method.GetContentList()).ReturnsAsync(() => actual);
+            var expected = await sut.GetContentList();
+
             //Then
+            Assert.Equal(expected, actual);
+
         }
     }
 }
