2239d0205ce7a59491c033620a2f83a06109c63f
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-09 18:19:56 -0300

    test(ContentRepository): ensure it returns a list of content on success

---
+++ Blog.UnitTests/Contents/ContentRepositoryTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using Xunit;
+using Moq;
+using MockQueryable.Moq;
+
+using Blog.Domain.Contents.Entities;
+using Blog.Infrastructure.SqlServer.Contexts;
+using Blog.Infrastructure.SqlServer.Contents;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.UnitTests.Contents
+{
+    public class ContentRepositoryTests
+    {
+        private readonly Mock<ContentDataContext> dataContextMock;
+
+        public ContentRepositoryTests()
+        {
+            var inMemoryOptions = new DbContextOptionsBuilder<ContentDataContext>()
+                    .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                    .Options;
+            this.dataContextMock = new Mock<ContentDataContext>(MockBehavior.Default, new[] { inMemoryOptions });
+        }
+
+        [Fact]
+        public async Task ShouldReturnListOfContentOnSuccess()
+        {
+            //Given
+            var contents = new List<Content>();
+            var mock = contents.AsQueryable().BuildMockDbSet();
+            var sut = new ContentRepository(this.dataContextMock.Object);
+
+            //When
+            this.dataContextMock.SetupGet(property => property.Contents).Returns(mock.Object);
+            var expected = await sut.GetContentList();
+
+            //Then
+            Assert.Equal(expected, contents);
+        }
+    }
+}
