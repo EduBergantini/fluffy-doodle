f2298ec78e462227d6d71bb45dc83538eeb83c93
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-09 22:38:53 -0300

    feat(ContentsController): ensure it returns a content list when http get returns 200

---
+++ Blog.Server.Api/Controllers/ContentsController.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Domain.Contents.UseCases;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blog.Server.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentsController : ControllerBase
+    {
+        private readonly IGetContentListUseCase getContentListUseCase;
+
+        public ContentsController(IGetContentListUseCase getContentListUseCase)
+        {
+            this.getContentListUseCase = getContentListUseCase;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var contents = await this.getContentListUseCase.GetContentList();
+            return Ok(contents);
+
+        }
+    }
+}
