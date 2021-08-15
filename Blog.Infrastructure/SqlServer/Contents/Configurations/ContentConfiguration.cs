de8f7dd210efcfd36e2cddb77311e013fc1ff4bc
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-15 17:22:47 -0300

    feat(IGetContentRepository): ensure it returns a content when GetContent succeeds

--- Blog.Infrastructure/SqlServer/Contents/Configurations/ContentConfiguration.cs
+++ Blog.Infrastructure/SqlServer/Contents/Configurations/ContentConfiguration.cs
@@ -13,6 +13,7 @@ namespace Blog.Infrastructure.SqlServer.Contents.Configurations
             builder.HasKey(content => content.Id).HasName("PK_CONTENTS");
             builder.Property(content => content.Id).HasColumnName("CTNT_ID").IsRequired();
             builder.Property(content => content.Title).HasColumnName("CTNT_TITLE").IsRequired().HasMaxLength(120);
+            builder.Property(content => content.PublicId).HasColumnName("CTNT_PUBID").IsRequired().HasMaxLength(150);
             builder.Property(content => content.Subtitle).HasColumnName("CTNT_SUBTITLE").IsRequired().HasMaxLength(270);
             builder.Property(content => content.FeaturedImage).HasColumnName("CTNT_FEATIMG").IsRequired().HasMaxLength(200);
         }
--- Blog.Infrastructure/SqlServer/Contents/ContentRepository.cs
+++ Blog.Infrastructure/SqlServer/Contents/ContentRepository.cs
@@ -6,16 +6,23 @@ using Microsoft.EntityFrameworkCore;
 using Blog.Application.Contents.Protocols;
 using Blog.Domain.Contents.Entities;
 using Blog.Infrastructure.SqlServer.Contexts;
+using System.Linq.Expressions;
+using System;
 
 namespace Blog.Infrastructure.SqlServer.Contents
 {
-    public class ContentRepository : IGetContentListRepository
+    public class ContentRepository : IGetContentListRepository, IGetContentRepository
     {
         private readonly ContentDataContext dataContext;
 
         public ContentRepository(ContentDataContext dataContext)
         {
             this.dataContext = dataContext;
+        }
+
+        public Task<Content> GetContent(Expression<Func<Content, bool>> predicate)
+        {
+            return this.dataContext.Contents.FirstOrDefaultAsync(predicate);
         }
 
         public async Task<IEnumerable<Content>> GetContentList()
