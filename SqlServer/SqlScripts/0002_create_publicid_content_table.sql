1ce4bcec4f5e30f75a0d4a376872f2db8e4b9298
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-15 21:50:02 -0300

    feat: include public id column to contents table script migration

---
+++ SqlServer/SqlScripts/0002_create_publicid_content_table.sql
@@ -0,0 +1,13 @@
+﻿BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [contents].[TBL_CONTENTS] ADD [CTNT_PUBID] nvarchar(150) NOT NULL DEFAULT N'';
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210815213439_CreatePublicIdForContents', N'5.0.8');
+GO
+
+COMMIT;
+GO
+
