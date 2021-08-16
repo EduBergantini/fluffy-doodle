403a125bf647bf7e7c3b48410104f4fb76be18ab
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-15 21:47:52 -0300

    feat: include public id column to contents table code migration

---
+++ Blog.Infrastructure/SqlServer/Migrations/20210815213439_CreatePublicIdForContents.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Infrastructure.SqlServer.Migrations
+{
+    public partial class CreatePublicIdForContents : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CTNT_PUBID",
+                schema: "contents",
+                table: "TBL_CONTENTS",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CTNT_PUBID",
+                schema: "contents",
+                table: "TBL_CONTENTS");
+        }
+    }
+}
