7f0015c786ce9abce95e5dc50c37ff6e7d91ef1c
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-20 16:59:46 -0300

    fix: ensure integration test does not depends on container database

--- Blog.IntegrationTests/Common/ApiWebApplicationFactory.cs
+++ Blog.IntegrationTests/Common/ApiWebApplicationFactory.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 
 using Blog.Server.Api;
 
+
 namespace Blog.IntegrationTests.Common
 {
-    public abstract class ApiWebApplicationFactory : WebApplicationFactory<Startup>
+    public abstract class ApiWebApplicationFactory : WebApplicationFactory<Startup>, IDisposable
     {
+        protected IConfiguration Configuration { get; set; }
+
     }
 }
--- Blog.IntegrationTests/Contents/Factories/ContentsIntegrationTest.cs
+++ Blog.IntegrationTests/Contents/Factories/ContentsIntegrationTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
@@ -7,12 +8,19 @@ using Blog.IntegrationTests.Common;
 using Blog.IntegrationTests.Contents.Stubs;
 using Blog.Domain.Contents.UseCases.Contents;
 
+
 namespace Blog.IntegrationTests.Contents.Factories
 {
     public class ContentsIntegrationTest : ApiWebApplicationFactory
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            builder.ConfigureAppConfiguration(config => 
+            { 
+                Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.Integration.json").Build(); 
+                config.AddConfiguration(Configuration); 
+            });
+
             builder.ConfigureTestServices(services =>
             {
                 services.AddScoped<IGetContentListUseCase, ContentListUseCaseStub>();
