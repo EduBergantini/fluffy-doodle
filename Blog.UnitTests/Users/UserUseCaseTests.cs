5d247b62071cd1f253a11f615b066a24cc6a17c3
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-21 16:16:26 -0300

    test(IAuthenticateUseCase): ensure it calls IGetUserByEmailRepository with correct values

---
+++ Blog.UnitTests/Users/UserUseCaseTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+using Xunit;
+using Moq;
+
+using Blog.Application.Users.UseCases;
+
+using Blog.Application.Users.Protocols;
+using Blog.Domain.Users.Entities;
+
+namespace Blog.UnitTests.Users
+{
+    public class UserUseCaseTests
+    {
+
+        [Fact]
+        public async Task ShouldCallGetUserByEmailRepositoryWithCorrectValues()
+        {
+            var mockedGetByEmailRepository = new Mock<IGetUserByEmailRepository>(MockBehavior.Default);
+            var sut = new UserUseCase(mockedGetByEmailRepository.Object);
+            var actual = "any_mail";
+            string emailParameter = null;
+
+            mockedGetByEmailRepository.Setup(method => method.GetByEmail(It.IsAny<string>()))
+                .Callback<string>((email) => emailParameter = email)
+                .ReturnsAsync(new User());
+
+            var user = await sut.Authenticate(actual, "any_password");
+
+            Assert.Equal(emailParameter, actual);
+        }
+    }
+}
