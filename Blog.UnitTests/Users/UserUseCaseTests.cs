732862e9a6e4a326694a9152a181f455bd5c9f1a
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-21 16:22:45 -0300

    refactor(UserUseCaseTests): move sut to the constructor to reuse in all tests

--- Blog.UnitTests/Users/UserUseCaseTests.cs
+++ Blog.UnitTests/Users/UserUseCaseTests.cs
@@ -12,22 +12,31 @@ namespace Blog.UnitTests.Users
 {
     public class UserUseCaseTests
     {
+        private readonly Mock<IGetUserByEmailRepository> mockedGetByEmailRepository;
+        private readonly UserUseCase sut;
+
+        public UserUseCaseTests()
+        {
+            this.mockedGetByEmailRepository = new Mock<IGetUserByEmailRepository>(MockBehavior.Default);
+            this.sut = new UserUseCase(mockedGetByEmailRepository.Object);
+        }
 
         [Fact]
         public async Task ShouldCallGetUserByEmailRepositoryWithCorrectValues()
         {
-            var mockedGetByEmailRepository = new Mock<IGetUserByEmailRepository>(MockBehavior.Default);
-            var sut = new UserUseCase(mockedGetByEmailRepository.Object);
+            
             var actual = "any_mail";
             string emailParameter = null;
 
-            mockedGetByEmailRepository.Setup(method => method.GetByEmail(It.IsAny<string>()))
+            this.mockedGetByEmailRepository.Setup(method => method.GetByEmail(It.IsAny<string>()))
                 .Callback<string>((email) => emailParameter = email)
                 .ReturnsAsync(new User());
 
-            var user = await sut.Authenticate(actual, "any_password");
+            var user = await this.sut.Authenticate(actual, "any_password");
 
             Assert.Equal(emailParameter, actual);
         }
+
+
     }
 }
