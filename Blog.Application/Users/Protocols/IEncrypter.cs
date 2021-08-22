55fa11656c493523e302c690c8009e6093f5b73b
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-22 01:15:21 -0300

    feat(IAuthenticateUseCase): ensure it calls IEnrypter with correct values

---
+++ Blog.Application/Users/Protocols/IEncrypter.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Blog.Application.Users.Protocols
+{
+    public interface IEncrypter
+    {
+        Task<string> Encrypt(string value);
+    }
+}
--- Blog.Application/Users/UseCases/UserUseCase.cs
+++ Blog.Application/Users/UseCases/UserUseCase.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 using Blog.Application.Users.Protocols;
 using Blog.Domain.Users.Entities;
@@ -11,16 +10,22 @@ namespace Blog.Application.Users.UseCases
     public class UserUseCase : IAuthenticateUseCase
     {
         private readonly IGetUserByEmailRepository getUserByEmailRepository;
+        private readonly IEncrypter encrypter;
 
-        public UserUseCase(IGetUserByEmailRepository getUserByEmailRepository)
+        public UserUseCase(
+            IGetUserByEmailRepository getUserByEmailRepository,
+            IEncrypter encrypter
+        )
         {
             this.getUserByEmailRepository = getUserByEmailRepository;
+            this.encrypter = encrypter;
         }
 
         public async Task<User> Authenticate(string email, string password)
         {
             var user = await this.getUserByEmailRepository.GetByEmail(email);
             if (user == null) throw new UserNotFoundException();
+            await this.encrypter.Encrypt(password);
             return user;
         }
     }
