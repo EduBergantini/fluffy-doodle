a8a1617fdb48ccfc6f5946c3391b224d600c8ce9
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-22 19:38:01 -0300

    feat(SimpleHashingAdapter): ensure it returns true when Verify returns true

---
+++ Blog.Application/Common/Protocols/ICompareHash.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Blog.Application.Common.Protocols
+{
+    public interface ICompareHash
+    {
+        Task<bool> CompareHash(string plainValue, string hashedValue);
+    }
+}
--- Blog.Infrastructure/Common/Adapters/SimpleHashingAdapter.cs
+++ Blog.Infrastructure/Common/Adapters/SimpleHashingAdapter.cs
@@ -7,13 +7,19 @@ using Blog.Application.Common.Protocols;
 
 namespace Blog.Infrastructure.Common.Adapters
 {
-    public class SimpleHashingAdapter : ICreateHash
+    public class SimpleHashingAdapter : ICreateHash, ICompareHash
     {
         private readonly ISimpleHash simpleHash;
 
         public SimpleHashingAdapter(ISimpleHash simpleHash)
         {
             this.simpleHash = simpleHash;
+        }
+
+        public Task<bool> CompareHash(string plainValue, string hashedValue)
+        {
+            var result = this.simpleHash.Verify(plainValue, hashedValue);
+            return Task.FromResult(result);
         }
 
         public Task<string> CreateHash(string value, int interations)
