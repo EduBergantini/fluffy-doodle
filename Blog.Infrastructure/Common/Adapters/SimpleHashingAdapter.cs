780ce066aa767f3c958073844dbe297bdff1968b
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-22 14:46:47 -0300

    feat(hashedPassword): ensure it returns a valid hashed password

---
+++ Blog.Infrastructure/Common/Adapters/SimpleHashingAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using SimpleHashing.Net;
+
+using Blog.Application.Common.Protocols;
+
+
+namespace Blog.Infrastructure.Common.Adapters
+{
+    public class SimpleHashingAdapter : IApplicationHasher
+    {
+        private readonly ISimpleHash simpleHash;
+
+        public SimpleHashingAdapter(ISimpleHash simpleHash)
+        {
+            this.simpleHash = simpleHash;
+        }
+
+        public Task<string> Hash(string value)
+        {
+            var hashedPassword = this.simpleHash.Compute(value);
+            return Task.FromResult(hashedPassword);
+        }
+    }
+}
