5e9506fe3229c9c9245dff0926bb42ac1629cd40
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-22 14:46:56 -0300

    test(hashedPassword): ensure it returns a valid hashed password

---
+++ Blog.UnitTests/Common/SimpleHashingAdapterTests.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Infrastructure.Common.Adapters;
+using Moq;
+using SimpleHashing.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Blog.UnitTests.Common
+{
+    public class SimpleHashingAdapterTests
+    {
+        [Fact]
+        public async Task ShouldReturnValidHashedPassword()
+        {
+            var mockedSimpleHash = new Mock<ISimpleHash>(MockBehavior.Default);
+            var sut = new SimpleHashingAdapter(mockedSimpleHash.Object);
+            var hashedPassword = "hashed_password";
+
+            mockedSimpleHash.Setup(method => method.Compute(It.IsAny<string>())).Returns("hashed_password");
+
+            var expected = await sut.Hash("any_password");
+
+            Assert.Equal(expected, hashedPassword);
+        }
+    }
+}
