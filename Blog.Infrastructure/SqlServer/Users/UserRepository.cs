32edb13c014daddff4b9576822aed78ed0dbdc35
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-23 15:10:32 -0300

    feat(UserRepository): ensure it returns a valid user when GetUserByEmail succeeds

---
+++ Blog.Infrastructure/SqlServer/Users/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Blog.Application.Users.Protocols;
+using Blog.Domain.Users.Entities;
+using Blog.Infrastructure.SqlServer.Contexts;
+
+namespace Blog.Infrastructure.SqlServer.Users
+{
+    public class UserRepository : IGetUserByEmailRepository
+    {
+        private readonly ContentDataContext dataContext;
+
+        public UserRepository(ContentDataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+        public Task<User> GetByEmail(string email)
+        {
+            var user = this.dataContext.Users.SingleAsync(user => user.Email == email);
+            return user;
+        }
+    }
+}
