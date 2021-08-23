f351d7921b9783800180017de722e8abed37f9eb
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-23 15:10:43 -0300

    test(UserRepository): ensure it returns a valid user when GetUserByEmail succeeds

--- Blog.UnitTests/Users/Fakes/UserFake.cs
+++ Blog.UnitTests/Users/Fakes/UserFake.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using Bogus;
 
 using Blog.Domain.Users.Entities;
+using Blog.Domain.Roles.Entities;
+using Blog.Domain.Status.Entities;
 
 namespace Blog.UnitTests.Users.Fakes
 {
@@ -20,14 +23,30 @@ namespace Blog.UnitTests.Users.Fakes
             return new User
             {
                 Id = this.faker.Internet.Random.Int(min: 0),
+                PublicId = this.faker.Random.Uuid().ToString(),
                 FullName = this.faker.Name.FullName(),
                 Email = this.faker.Internet.Email(),
                 Password = this.faker.Internet.Password(),
+                RoleId = 1,
+                Role = new ApplicationRole { Id = 1, Description = "Administrador"},
+                StatusId = 1,
+                Status = new ApplicationStatus { Id = 1, Description = "Ativo"},
                 RegisterDate = DateTime.UtcNow
             };
-        } 
+        }
 
-        public static User GetUser(string password)
+        internal static IEnumerable<User> GetUserList(int count = 3)
+        {
+            var users = new List<User>();
+            for (int i = 0; i < count; i++)
+            {
+                var userFake = new UserFake();
+                users.Add(userFake.GetFakeUser());
+            }
+            return users;
+        }
+
+        internal static User GetUser(string password)
         {
             var fakeUser = new UserFake();
             var user = fakeUser.GetFakeUser();
--- Blog.UnitTests/Users/UserRepositoryTests.cs
+++ Blog.UnitTests/Users/UserRepositoryTests.cs
@@ -1,14 +1,42 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MockQueryable.Moq;
+using Xunit;
+
+using Blog.Domain.Users.Entities;
+using Blog.Infrastructure.SqlServer.Users;
 using Blog.UnitTests.Common;
+using Blog.UnitTests.Users.Fakes;
 
 namespace Blog.UnitTests.Users
 {
     public class UserRepositoryTests : CommonRepositoryTest
     {
+        private readonly IEnumerable<User> users;
+        private readonly UserRepository sut;
+
         public UserRepositoryTests()
             : base()
         {
+            this.users = UserFake.GetUserList();
+            this.sut = new UserRepository(base.DataContextMock.Object);
+        }
 
+        [Fact]
+        public async Task ShouldReturnValidUserWhenGetByEmailSucceeds()
+        {
+            //Given
+            var mock = this.users.AsQueryable().BuildMockDbSet();
+            var actual = this.users.First();
+
+            //When
+            base.DataContextMock.SetupGet(property => property.Users).Returns(mock.Object);
+            var expected = await sut.GetByEmail(actual.Email);
+
+            //Then
+            Assert.Equal(expected, actual);
         }
     }
 }
