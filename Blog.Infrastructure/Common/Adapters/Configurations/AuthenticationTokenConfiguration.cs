b47950a4e4d52a439a2c6beb36acd544c126fb1b
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-27 17:53:40 -0300

    feat(IdentityJwtAdapter): ensure CreateToken returns a valid token

---
+++ Blog.Infrastructure/Common/Adapters/Configurations/AuthenticationTokenConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Blog.Infrastructure.Common.Adapters.Configurations
+{
+    public class AuthenticationTokenConfiguration
+    {
+        public string SecretKey { get; }
+        public int TotalDaysToExpire { get; }
+        public DateTime now;
+
+        public AuthenticationTokenConfiguration(string secretKey, int totalDaysToExpire = 7)
+        {
+            SecretKey = secretKey;
+            TotalDaysToExpire = totalDaysToExpire;
+            this.ExpirationDate = DateTime.UtcNow.AddDays(this.TotalDaysToExpire);
+            this.now = DateTime.UtcNow;
+        }
+
+        public DateTime ExpirationDate { get; }
+
+        public int ExpireInMs
+        {
+            get
+            {
+                return (int)ExpirationDate.Subtract(this.now).TotalMilliseconds;
+            }
+        }
+    }
+}
---
+++ Blog.Infrastructure/Common/Adapters/IdentityJwtAdapter.cs
@@ -0,0 +1,47 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.IdentityModel.Tokens;
+
+using Blog.Application.Common.Protocols;
+using Blog.Domain.Users.Models;
+using Blog.Infrastructure.Common.Adapters.Configurations;
+
+namespace Blog.Infrastructure.Common.Adapters
+{
+    public class IdentityJwtAdapter : ICreateEncryption
+    {
+        private readonly AuthenticationTokenConfiguration authenticationTokenConfiguration;
+
+        public IdentityJwtAdapter(AuthenticationTokenConfiguration authenticationTokenConfiguration)
+        {
+            this.authenticationTokenConfiguration = authenticationTokenConfiguration;
+        }
+
+        public Task<AuthenticationTokenModel> CreateToken(int userId, int roleId)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            //var key = Encoding.ASCII.GetBytes("[SECRET USED TO SIGN AND VERIFY JWT TOKENS, IT CAN BE ANY STRING]");
+            var key = Encoding.UTF32.GetBytes(this.authenticationTokenConfiguration.SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.PrimarySid, userId.ToString()),
+                    new Claim(ClaimTypes.Role, roleId.ToString()),
+                }),
+                Expires = this.authenticationTokenConfiguration.ExpirationDate,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var authModel = new AuthenticationTokenModel
+            {
+                Token = tokenHandler.WriteToken(token),
+                ExpireInMs = this.authenticationTokenConfiguration.ExpireInMs
+            };
+            return Task.FromResult(authModel);
+        }
+    }
+}
