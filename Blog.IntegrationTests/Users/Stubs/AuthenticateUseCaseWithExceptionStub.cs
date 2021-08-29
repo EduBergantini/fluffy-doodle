c817a4694f171726aa7e454dbf6fb36ed15766b4
Eduardo B. Pinto <eduardo_bergantini_pinto@hotmail.com>
2021-08-28 22:47:31 -0300

    test(UsersController): ensure it returns bad request when user is not found

---
+++ Blog.IntegrationTests/Users/Stubs/AuthenticateUseCaseWithExceptionStub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Blog.Domain.Users.Models;
+using Blog.Domain.Users.UseCases;
+
+namespace Blog.IntegrationTests.Users.Stubs
+{
+    public class AuthenticateUseCaseWithExceptionStub : IAuthenticateUseCase
+    {
+        private readonly Exception exception;
+
+        public AuthenticateUseCaseWithExceptionStub(Exception exception)
+        {
+            this.exception = exception;
+        }
+
+        public Task<AuthenticationTokenModel> Authenticate(string email, string password)
+        {
+            throw exception;
+        }
+    }
+}
--- Blog.IntegrationTests/Users/UsersControllerTests.cs
+++ Blog.IntegrationTests/Users/UsersControllerTests.cs
@@ -1,16 +1,22 @@
-﻿using Blog.Domain.Users.Models;
-using Blog.IntegrationTests.Common;
-using Blog.IntegrationTests.Users.Factories;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System;
-using System.Collections.Generic;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
+using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+
 using Xunit;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+using Blog.Domain.Users.Models;
+using Blog.IntegrationTests.Common;
+using Blog.IntegrationTests.Users.Factories;
+using Blog.Domain.Users.UseCases;
+using Blog.IntegrationTests.Users.Stubs;
+using Blog.Domain.Users.Errors;
+using System.Collections.Generic;
+
 
 namespace Blog.IntegrationTests.Users
 {
@@ -26,6 +32,41 @@ namespace Blog.IntegrationTests.Users
         }
 
         [Fact]
+        public async Task POST_ShouldReturnBadRequestWhenSignReceiveInvalidModel()
+        {
+            var response = await base.httpClient.PostAsync(this.signInUrl, new StringContent("{}", Encoding.UTF8, "application/json"));
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            Assert.False(string.IsNullOrWhiteSpace(body));
+        }
+
+        [Fact]
+        public async Task POST_ShouldReturnServerErrorWhenSignInThrowsError()
+        {
+            var client = base.factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddScoped<IAuthenticateUseCase, AuthenticateUseCaseWithExceptionStub>((service) =>
+                    {
+                        return new AuthenticateUseCaseWithExceptionStub(new UserNotFoundException());
+                    });
+                });
+            }).CreateClient();
+
+            var response = await client.PostAsync(this.signInUrl, this.validHttpContent);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+            var body = await response.Content.ReadAsStringAsync();
+            Assert.False(string.IsNullOrWhiteSpace(body));
+
+            var error = JsonSerializer.Deserialize<IDictionary<string, string[]>>(body);
+            Assert.Single(error.First().Value);
+            Assert.Equal("Senha inválida ou o usuário não encontrado", error.First().Value[0]);
+        }
+
+        [Fact]
         public async Task POST_ShouldReturnAuthenticationTokenWhenSignInSucceeds()
         {
             var response = await base.httpClient.PostAsync(this.signInUrl, validHttpContent);
@@ -35,16 +76,6 @@ namespace Blog.IntegrationTests.Users
             var authToken = JsonSerializer.Deserialize<AuthenticationTokenModel>(body, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             Assert.Equal("any_token", authToken.Token);
             Assert.Equal(1000, authToken.ExpireInMs);
-        }
-
-        [Fact]
-        public async Task POST_ShouldReturnBadRequestWhenSignReceiveInvalidModel()
-        {
-            var response = await base.httpClient.PostAsync(this.signInUrl, new StringContent("{}", Encoding.UTF8, "application/json"));
-            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-
-            var body = await response.Content.ReadAsStringAsync();
-            Assert.False(string.IsNullOrWhiteSpace(body));
         }
     }
 }
